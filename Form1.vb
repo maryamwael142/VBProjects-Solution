170d7ceb9de2e91e2a5dfd7eadc97106ded8bdc2
maryamwael142 <maryamwael544@gmail.com>
2025-11-20 21:54:34 +0200

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -2,11 +2,6 @@
 
 Public Class Form1
 
-    Private Sub label1_click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label4.Text = TextBox1.Text & " " & TextBox2.Text
     End Sub
@@ -22,5 +17,6 @@ Public Class Form1
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Application.Exit()
     End Sub
+
 
 End Class
