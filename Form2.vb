da362764ed328fadff1f432711197f60e5650e00
maryamwael142 <maryamwael544@gmail.com>
2025-11-20 21:56:38 +0200

    Implement form load and button click functionality
    Added event handlers for loading the form and button clicks.

--- Form2.vb
+++ Form2.vb
@@ -1,3 +1,22 @@
-﻿Public Class Form2
+Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label4.Text = Val(TextBox1.Text) + Val(TextBox2.Text)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        Label4.Text = ""
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
 
 End Class
+
