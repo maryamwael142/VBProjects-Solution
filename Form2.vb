7214c3b39f941251c875879ca25a310911755677
maryamwael142 <maryamwael544@gmail.com>
2025-11-21 02:04:54 +0200

    Remove redundant Form2_Load method

--- Form2.vb
+++ Form2.vb
@@ -1,7 +1,5 @@
 Public Class Form2
-    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
+    
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label4.Text = Val(TextBox1.Text) + Val(TextBox2.Text)
 
@@ -19,4 +17,5 @@ Public Class Form2
     End Sub
 
 End Class
+
 
