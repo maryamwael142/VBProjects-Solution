5133aca81641f4119683df9b0decb3c45011f7e8
maryamwael142 <maryamwael544@gmail.com>
2025-11-21 03:54:48 +0200

    Add files via upload

---
+++ Form34.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form34
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i As Integer
+        Dim sum As Integer
+        sum = 0
+
+        For i = 0 To 100 Step 2
+            sum = sum + i
+        Next i
+        Label2.Text = sum
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+End Class
