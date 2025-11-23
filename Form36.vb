1909f1d82fd3f88af17ff7238e728fee15ca28d6
maryamwael142 <maryamwael544@gmail.com>
2025-11-23 21:49:10 +0200

    Add files via upload

---
+++ Form36.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form36
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim N As String
+        N = InputBox("enter name", "Name")
+        MsgBox("welcome ", vbOKOnly, "hello")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+
+    End Sub
+
+
+
+
+
+
+
+
+
+
+End Class
+
+
---
+++ Form37.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form37
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim N As String
+        N = InputBox("your age is ", "age")
+        MsgBox("welcome " + N, vbOKOnly, "hello")
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+End Class
