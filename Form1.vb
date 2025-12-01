cc5c141052157460318a1517c4c662f9062fbce4
maryamwael142 <maryamwael544@gmail.com>
2025-12-01 23:02:01 +0200

    Fix exit method in Button3_Click event

--- Form1.vb
+++ Form1.vb
@@ -15,8 +15,9 @@ Public Class Form1
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        Application.Exit()
+        End
     End Sub
 
 
 End Class
+
