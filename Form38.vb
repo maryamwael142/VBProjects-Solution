eefa8009343de3f1dfc65b5c0e9c32cb9aee566d
maryamwael142 <maryamwael544@gmail.com>
2025-12-05 15:38:46 +0200

    Add files via upload

---
+++ Form38.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i As Integer
+        For i = 1 To 10 Step 2
+            i = i + 1
+        Next
+        ListBox1.Items.Add(i)
+
+    End Sub
+
+    Private Sub button2_click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+
+    End Sub
+End Class
