6d4f9f0f6c2936ac9f4190f339c16418ba012413
maryamwael142 <maryamwael544@gmail.com>
2025-12-05 16:33:38 +0200

    Rename Form1 to Form38 and retain functionality

--- Form38.vb
+++ Form38.vb
@@ -1,4 +1,4 @@
-﻿Public Class Form1
+﻿Public Class Form38
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim i As Integer
         For i = 1 To 10 Step 2
@@ -13,3 +13,4 @@
 
     End Sub
 End Class
+
