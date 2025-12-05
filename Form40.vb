13a87af91d6e3510f62497b68a278831045a4153
maryamwael142 <maryamwael544@gmail.com>
2025-12-05 18:21:39 +0200

    Add files via upload

---
+++ Form40.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form40
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As String
+        n = InputBox("Enter name", "Name")
+
+        If n = "rami" Then
+            ListBox1.Items.Add("true")
+        Else
+            ListBox1.Items.Add("false")
+        End If
+
+    End Sub
+    Private Sub button2_click(sender As Object, e As EventArgs) Handles button2.click
+        End
+
+    End Sub
+End Class
