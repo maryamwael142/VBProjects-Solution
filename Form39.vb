22d14c20eac3ab79556206352db230d1ee85d8c2
maryamwael142 <maryamwael544@gmail.com>
2025-12-05 17:44:01 +0200

    Add files via upload

---
+++ Form39.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form39
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim x, y As Integer
+        x = InputBox("enter x", "x")
+        y = InputBox("enter y", "y")
+        If x > y Then
+            MsgBox("yes")
+        Else
+            MsgBox("no")
+        End If
+        ListBox1.Items.Add("x=" & x & " y=" & y)
+    End Sub
+
+
+    Private Sub button2_click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+
+    End Sub
+End Class
