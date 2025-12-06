de7d79fa8640b525ba810b094f2768f2eda63330
maryamwael142 <maryamwael544@gmail.com>
2025-12-06 16:07:56 +0200

    Add files via upload

---
+++ Form46.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form46
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim f As Integer = 1
+
+        For i As Integer = 1 To 7
+            f = f * i
+        Next
+
+        Label2.Text = "7! = " & f
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+End Class
