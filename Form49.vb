b14398a136b87849120dc9cf53ae0ccf2b15c4f1
maryamwael142 <maryamwael544@gmail.com>
2025-12-06 17:23:59 +0200

    Add files via upload

---
+++ Form49.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form49
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i, f, n As Integer
+        f = 1
+        i = 1
+
+100:    n = InputBox("enter number", "factorial")
+        If n > 10 Then
+            GoTo 100
+        Else
+            Do Until i > n
+                f = f * i
+                i = i + 1
+
+            Loop
+            Label2.Text = f
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+End Class
