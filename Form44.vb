f00eb0decd2377d0abc4c7f8e5cf59821b54f451
maryamwael142 <maryamwael544@gmail.com>
2025-12-06 14:18:09 +0200

    Add files via upload

---
+++ Form44.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form44
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x As Integer
+
+        x = InputBox("enter x ", "x")
+        If x > 10 Then
+            Label2.Text = x & "  True"
+        Else
+            Label2.Text = x & "  False"
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+End Class
