ef16c2cbebd09c379bd9dd1b3c87f9bd2f77685f
maryamwael142 <maryamwael544@gmail.com>
2025-12-06 14:58:11 +0200

    Add files via upload

---
+++ Form45.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form45
+    Private Sub button1_click(sender As Object, e As EventArgs) Handles button1.click
+        Dim n As String
+        n = InputBox("enter name", "name")
+        If n = "Tia" Then
+            MsgBox("Program will end ")
+
+        End If
+
+        Label2.Text = n
+    End Sub
+    Private Sub button2_click(sender As Object, e As EventArgs) Handles button2.click
+        End
+    End Sub
+
+End Class
