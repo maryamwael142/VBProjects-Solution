cbb49322a0b6edd9270910c37fde5740ae35d9a6
maryamwael142 <maryamwael544@gmail.com>
2025-12-13 20:05:57 +0200

    Add files via upload

---
+++ Form50.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form50
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer
+
+        Do
+            n = Val(InputBox("Enter a number (<= 10)", "Input"))
+        Loop While n > 10
+
+        Label2.Text = "The number is: " & n
+    End Sub
+
+        Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+            End
+        End Sub
+    End Class
+
+
