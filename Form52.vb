af7c244febf972ce494c8e17450388187ea568d7
maryamwael142 <maryamwael544@gmail.com>
2025-12-15 18:17:47 +0200

    Add files via upload

---
+++ Form52.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form52
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i As Integer
+        Dim result As String = ""
+
+        For i = 1 To 10
+            result &= "تحيا مصر "
+        Next
+
+        Label1.Text = result
+
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+End Class
